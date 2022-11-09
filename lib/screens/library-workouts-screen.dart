14ea63a2d8fe624e55d38bbf81f14c833c040f8b
Robert Myers <robbamyers@icloud.com>
2022-11-08 20:30:48 -0500

    added saveButton to new workout screen, added workout lists

--- lib/screens/library-workouts-screen.dart
+++ lib/screens/library-workouts-screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LibraryWorkoutScreen extends StatelessWidget {
-  const LibraryWorkoutScreen({Key? key}) : super(key: key);
+import '../classes/config.dart';
+import '../classes/workout.dart';
+import '../providers/config-provider.dart';
 
+class LibraryWorkoutScreen extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final List<Workout> workouts = ref.watch(configProvider).library.workouts;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Library/Workouts'),
@@ -15,6 +20,39 @@ class LibraryWorkoutScreen extends StatelessWidget {
         },
         child: const Icon(Icons.add),
       ),
+      body: Center(
+        child: Column(
+          children: generateButtons(workouts, context, ref),
+        ),
+      ),
     );
+  }
+
+  List<Widget> generateButtons(List<Workout> workouts, BuildContext context, WidgetRef ref) {
+    List<Widget> listOfButtons = [];
+    workouts.sort((a, b) {
+      if (a.getFavorite()) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+    for (Workout w in workouts) {
+      Icon fav = Icon(Icons.favorite_outline);
+      if (w.getFavorite()) {
+        fav = Icon(Icons.favorite);
+      }
+      ElevatedButton eb = ElevatedButton(
+          onPressed: () {},
+          child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+            IconButton(
+              onPressed: () {},
+              icon: fav,
+            ),
+            Text(w.getName()),
+          ]));
+      listOfButtons.add(Padding(padding: EdgeInsets.all(5), child: eb));
+    }
+    return listOfButtons;
   }
 }
